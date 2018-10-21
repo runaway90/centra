f681e60dfed9a5cd7a31792bf1aa4f578d5e99c4
vbezgin <vbezgin@localhost.localdomain>
2018-10-22 01:59:03 +0200

    Added create table function... //Without correct data and not tested at

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 use App\Controllers\Request\GitHubClientController;
+use \App\Controllers\Kanban\TableController;
 
 require_once __DIR__ . '../../vendor/autoload.php';
 
 $request = new GitHubClientController('6092c700905421477657', '0cb18dea4d62c35183166c4518f8d309f5b1c5b8');
 $milestonesOfRepository = $request->getIssuesForRepository('runaway90','test4callpage');
 
-var_dump($milestonesOfRepository);
+$table = new TableController();
+$table->create($milestonesOfRepository);
+//var_dump($milestonesOfRepository);
