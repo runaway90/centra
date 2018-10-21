41227bb926b1206d84e83381371731480da9a0c4
vbezgin <vbezgin@localhost.localdomain>
2018-10-22 01:08:23 +0200

    Understood that milestone is characretistics aspect by issue. Change milistone param to issue param in object Repository

--- public/index.php
+++ public/index.php
@@ -3,29 +3,7 @@ use App\Controllers\Request\GitHubClientController;
 
 require_once __DIR__ . '../../vendor/autoload.php';
 
+$request = new GitHubClientController('6092c700905421477657', '0cb18dea4d62c35183166c4518f8d309f5b1c5b8');
+$milestonesOfRepository = $request->getIssuesForRepository('runaway90','test4callpage');
 
-$req = new GitHubClientController('6092c700905421477657', '0cb18dea4d62c35183166c4518f8d309f5b1c5b8');
-$repo = $req->getMilestonesForRepository('runaway90','test4callpage');
-$allRepo = $req->getAllRepositoriesNames('runaway90');
-$mile = $req->getAllMilestonesByOwner('runaway90');
-
-var_dump($allRepo);
-
-//
-//use KanbanBoard\GithubActual;
-//use KanbanBoard\Utilities;
-//
-//require '../badway/KanbanBoard/Github.php';
-//require '../badway/Utilities.php';
-//require '../badway/KanbanBoard/Authentication.php';
-//
-//$repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
-//$authentication = new \KanbanBoard\Login();
-//$token = $authentication->login();
-//$github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
-//$board = new \KanbanBoard\Application($github, $repositories, array('waiting-for-feedback'));
-//$data = $board->board();
-//$m = new Mustache_Engine(array(
-//	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
-//));
-//echo $m->render('index', array('milestones' => $data));
+var_dump($milestonesOfRepository);
